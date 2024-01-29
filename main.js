2db7ad6bdb65fabec4ad53e8291ddc7c6d6a9bf6
Yana <yana102hanan@gmail.com>
2024-01-30 00:06:40 +0200

    Add style to the ball

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 
 function onBallClick(elB) {
     const expandBy = 50
-    var elBallWidth = +elB.innerHTML
     const elBall = document.querySelector('.ball')
+
+    var elBallWidth = +elB.innerHTML
     elBall.style.width = +elBallWidth + expandBy + "px"
     elBall.style.height = +elBallWidth + expandBy + "px"
     elB.innerHTML = +elBallWidth + expandBy
