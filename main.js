c6f5967a6c851779648804aebb4959b6ee0b76da
Yana <yana102hanan@gmail.com>
2024-01-30 00:24:50 +0200

    Make a limit to ball expand size

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ function onBallClick(elB) {
     const elBall = document.querySelector('.ball')
 
     var elBallWidth = +elB.innerHTML
-    elBall.style.width = +elBallWidth + expandBy + "px"
-    elBall.style.height = +elBallWidth + expandBy + "px"
-    elB.innerHTML = +elBallWidth + expandBy
+    if (elBallWidth < 400) {
+        elBall.style.width = +elBallWidth + expandBy + "px"
+        elBall.style.height = +elBallWidth + expandBy + "px"
+        elB.innerHTML = +elBallWidth + expandBy
+    }
+    if (elBallWidth === 400) {
+        elB.innerHTML = 100
+        elBall.style.width = +elB.innerHTML + "px"
+        elBall.style.height = +elB.innerHTML + "px"
+    }
 }
