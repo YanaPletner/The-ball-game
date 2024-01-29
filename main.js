5f3a5602ea41ac2eeb9e8ac4fbac667f30d27457
Yana <yana102hanan@gmail.com>
2024-01-29 22:57:21 +0200

    Make the ball expand each click

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 "use strict"
 
-function onBallClick(elBall) {
-    console.log('ball clicked')
+function onBallClick(elB) {
+    const expandBy = 50
+    var elBallWidth = +elB.innerHTML
+    const elBall = document.querySelector('.ball')
+    elBall.style.width = +elBallWidth + expandBy + "px"
+    elBall.style.height = +elBallWidth + expandBy + "px"
+    elB.innerHTML = +elBallWidth + expandBy
 }
