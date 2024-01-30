cb0aad3a170eefd57fda639416152418fd346353
Yana <yana102hanan@gmail.com>
2024-01-30 12:29:09 +0200

    Make ball1 and ball2 swap sizes and colors

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
 "use strict"
 
-function onBallClick(elB, maxDiameter) {
+function onBallClick(elBall, maxDiameter) {
     const randomExpand = getRandomInt(20, 60)
-    var elBallWidth = elB.innerHTML
+    var elBallWidth = elBall.innerHTML
 
     if (elBallWidth < maxDiameter) {
-        elB.style.width = +elBallWidth + randomExpand + "px"
-        elB.style.height = +elBallWidth + randomExpand + "px"
-        elB.innerHTML = +elBallWidth + randomExpand
+        elBall.style.width = +elBallWidth + randomExpand + "px"
+        elBall.style.height = +elBallWidth + randomExpand + "px"
+        elBall.innerHTML = +elBallWidth + randomExpand
     }
     if (elBallWidth >= maxDiameter) {
-        elB.innerHTML = 100
-        elB.style.width = +elB.innerHTML + "px"
-        elB.style.height = +elB.innerHTML + "px"
+        elBall.innerHTML = 100
+        elBall.style.width = +elBall.innerHTML + "px"
+        elBall.style.height = +elBall.innerHTML + "px"
     }
-    changeBallColor(elB)
+    changeBallColor(elBall)
 }
 
 function changeBallColor(elB) {
     const randomColor = getRandomColor()
     elB.style.backgroundColor = randomColor
+}
+
+function swapColorAndSize(elBall) {
+    const elBall1 = document.querySelector('.ball-1')
+    const elBall2 = document.querySelector('.ball-2')
+
+    const elBall1Size = elBall1.innerHTML
+    const elBall2Size = elBall2.innerHTML
+    elBall1.style.width = +elBall2Size + 'px'
+    elBall1.style.height = +elBall2Size + 'px'
+
+    elBall1.innerHTML = elBall2Size
+    elBall2.innerHTML = elBall1Size
+
+    elBall2.style.width = +elBall1Size + 'px'
+    elBall2.style.height = +elBall1Size + 'px'
+
+    const elBall1Color = window.getComputedStyle(elBall1).backgroundColor
+    const elBall2Color = window.getComputedStyle(elBall2).backgroundColor
+    elBall1.style.backgroundColor = elBall2Color
+    elBall2.style.backgroundColor = elBall1Color
 }
