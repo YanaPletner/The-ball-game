d441bfd37cec106d1e64bba7d08535bbe7914a3c
Yana <yana102hanan@gmail.com>
2024-01-30 10:32:40 +0200

    Add getRandomColor function and change ball color each click

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 "use strict"
 
 function onBallClick(elB) {
-    // const expandBy = 50
-    const elBall = document.querySelector('.ball')
     const randomExpand = getRandomInt(20, 60)
-    console.log(randomExpand)
     var elBallWidth = +elB.innerHTML
+
     if (elBallWidth < 400) {
-        elBall.style.width = +elBallWidth + randomExpand + "px"
-        elBall.style.height = +elBallWidth + randomExpand + "px"
+        elB.style.width = +elBallWidth + randomExpand + "px"
+        elB.style.height = +elBallWidth + randomExpand + "px"
         elB.innerHTML = +elBallWidth + randomExpand
     }
     if (elBallWidth >= 400) {
         elB.innerHTML = 100
-        elBall.style.width = +elB.innerHTML + "px"
-        elBall.style.height = +elB.innerHTML + "px"
+        elB.style.width = +elB.innerHTML + "px"
+        elB.style.height = +elB.innerHTML + "px"
     }
+    changeBallColor(elB)
+}
+
+function changeBallColor(elB) {
+    const randomColor = getRandomColor()
+    elB.style.backgroundColor = randomColor
 }
