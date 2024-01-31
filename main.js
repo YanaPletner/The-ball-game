92c8505f33ecfea260d8b3f479ada0711c34eb5d
Yana <yana102hanan@gmail.com>
2024-01-31 10:26:13 +0200

    Make functions to do a click event,wich stops after 10 cycles or we mouse out the reset ball

--- main.js
+++ main.js
@@ -54,7 +54,6 @@ function reduceByRandomSize() {
     } else {
         getReducedBall(elBall1, randomReduce)
     }
-
     if (elBall2Width - randomReduce < minDiameter) {
         getMinDiameter(elBall2, minDiameter)
 
@@ -106,4 +105,36 @@ function getReducedBall(elBall, randomReduce) {
     elBall.style.width = elBallWidth - randomReduce + 'px'
     elBall.style.height = elBallWidth - randomReduce + 'px'
     elBall.innerHTML = elBallWidth - randomReduce
+}
+
+var gResetBallIntrvl
+var gCycleCount = 0
+var gMaxCycles = 10
+
+function startResetBallInterval() {
+
+    gResetBallIntrvl = setInterval(() => {
+        console.log('start intrvl')
+        const elBall1 = document.querySelector('.ball-1')
+        const elBall2 = document.querySelector('.ball-2')
+        const elBall3 = document.querySelector('.ball-3')
+        const elBall4 = document.querySelector('.ball-4')
+
+        elBall1.click()
+        elBall2.click()
+        elBall3.click()
+        elBall4.click()
+
+        gCycleCount++
+        if (gCycleCount >= gMaxCycles) clearInterval(gResetBallIntrvl)
+    }, 2000)
+}
+
+function mouseOverReset() {
+    gCycleCount = 0
+    setTimeout(startResetBallInterval, 2000)
+}
+
+function mouseoutReset() {
+    if (gResetBallIntrvl) clearInterval(gResetBallIntrvl)
 }
