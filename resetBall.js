33382f2034b333d98f8e76af1f5349380c685513
Yana <yana102hanan@gmail.com>
2024-01-31 14:31:23 +0200

    Add code for the reset ball to make the interval stop

--- resetBall.js
+++ resetBall.js
@@ -17,30 +17,34 @@ function reset() {
     elBall1.style.backgroundColor = 'rgb(189, 145, 188)'
     elBall2.style.backgroundColor = 'rgb(154, 123, 184)'
     elBody.style.backgroundColor = 'rgb(15, 15, 15)'
+    gResetBallIntrvl = false
 }
 
 function startResetBallInterval() {
+    if (!gResetBallIntrvl) {
+        gResetBallIntrvl = setInterval(() => {
+            console.log('start intrvl')
+            const elBall1 = document.querySelector('.ball-1')
+            const elBall2 = document.querySelector('.ball-2')
+            const elBall3 = document.querySelector('.ball-3')
+            const elBall4 = document.querySelector('.ball-4')
 
-    gResetBallIntrvl = setInterval(() => {
-        console.log('start intrvl')
-        const elBall1 = document.querySelector('.ball-1')
-        const elBall2 = document.querySelector('.ball-2')
-        const elBall3 = document.querySelector('.ball-3')
-        const elBall4 = document.querySelector('.ball-4')
+            elBall1.click()
+            elBall2.click()
+            elBall3.click()
+            elBall4.click()
 
-        elBall1.click()
-        elBall2.click()
-        elBall3.click()
-        elBall4.click()
-
-        gCycleCount++
-        if (gCycleCount >= gMaxCycles) clearInterval(gResetBallIntrvl)
-    }, 2000)
+            gCycleCount++
+            if (gCycleCount >= gMaxCycles) clearInterval(gResetBallIntrvl)
+        }, 2000)
+    }
 }
 
 function mouseOverReset() {
-    gCycleCount = 0
-    setTimeout(startResetBallInterval, 2000)
+    if (!gResetBallIntrvl) {
+        gCycleCount = 0
+        setTimeout(startResetBallInterval, 2000)
+    }
 }
 
 function mouseoutReset() {
